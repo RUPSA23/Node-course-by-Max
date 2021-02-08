fa08505b0f781f62a6e7a1eca0036bbaff729324
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2021-02-08 13:40:25 +0530

    Complete Mongoose

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// some change done
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -18,27 +19,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk('601455990d3626d94139a7c3')
+  User.findById('601d9181988574390cf9f5a2')
     .then(user => {
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        // req.user = user;
-        next();
+      req.user = user;
+      next();
     })
-    .catch(err => {
-        console.log(err);
-    });
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// app.use(errorController.get404);
+app.use(errorController.get404);
 
-mongoConnect((abc) => {
-    app.listen(4440);
-});
-
-
-
-
-
+mongoose
+  .connect(
+    'mongodb://anusuya:admin1234@cluster0-shard-00-00.e8pdt.mongodb.net:27017,cluster0-shard-00-01.e8pdt.mongodb.net:27017,cluster0-shard-00-02.e8pdt.mongodb.net:27017/shop?replicaSet=atlas-elz3up-shard-0&ssl=true&authSource=admin', { useNewUrlParser: true }
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Rupsa',
+          email: 'rupsa@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/admin.js
+++ controllers/admin.js
@@ -16,18 +16,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
- const product = new Product(
-  title, 
-  price,
-  description,
-  imageUrl,
-  null,
-  req.user._id
-);
+ const product = new Product({
+   title: title,
+   price: price,
+   description: description,
+   imageUrl: imageUrl,
+   userId: req.user
+ });
  product
  .save()
   .then(result => {
-    // console.log(result);
     console.log('Created Product');
    res.redirect('/admin/products');
   }).catch(err => {
@@ -41,7 +39,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
+  Product.findById(prodId)
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -62,16 +60,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-      const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl,
-        // new ObjectId(prodId)
-        prodId
-        );
-    product
-    .save()
+    Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -80,8 +76,11 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
+  // .select('title price -_id')
+  // .populate('userId', 'name')
     .then(products => {
+      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
@@ -96,7 +95,7 @@ exports.getProducts = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productIdm;
   console.log(prodId.length);
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCTS');
       res.redirect('/admin/products');
